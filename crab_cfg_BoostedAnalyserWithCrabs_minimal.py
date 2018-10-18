8909fa2e587e15e4ad0397947e9b6ef3d86b4510
skudella <simon.kudella@cern.ch>
2018-10-18 14:03:10 +0200

    init

---
+++ crab_cfg_BoostedAnalyserWithCrabs_minimal.py
@@ -0,0 +1,32 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = ?
+config.General.workArea = 'ZPrime_MC_minimal'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+#config.JobType.psetName = '/afs/cern.ch/user/s/skudella/CMSSW_8_0_12/src/BoostedTTH/BoostedAnalyzer/test/mkVLQ_jettoolbox_trees_2016_cfg.py'
+config.JobType.psetName = '/afs/cern.ch/user/s/skudella/CMSSW_8_0_26_patch2/src/BoostedTTH/BoostedAnalyzer/test/Zprime_ntuples_renormstuff.py'
+
+config.JobType.maxMemoryMB = 3000
+#config.Site.blacklist = ['T2_US_*']
+
+config.JobType.outputFiles = ?
+config.JobType.pyCfgParams = ?
+#config.JobType.inputFiles = ['/nfs/dust/cms/user/asaibel/CMSSWProjects/CMSSW_7_6_3/src/TTH/MEIntegratorStandalone/root']
+
+
+config.section_("Data")
+config.Data.inputDataset = ?
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+#config.Data.totalUnits = 1
+#config.Data.publishDbsUrl = 'phys03'
+config.Data.outputDatasetTag = ?
+
+config.section_("Site")
+config.Site.storageSite = 'T2_DE_DESY'
---
+++ generate_crabs_BoostedAnalyserWithCrabs_minimal.py
@@ -0,0 +1,68 @@
+import csv
+import os
+#import das_client
+import sys
+import imp
+import ssl
+import Utilities.General.cmssw_das_client as das_client
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+#das_client=imp.load_source("das_client", "/cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/das_client/v02.17.04/bin/das_client.py")
+
+crab_cfg_path='ZPrime_MC_minimal'
+ZPrime_MC_studies_tWb=sys.argv[1]
+
+csvfile=open(ZPrime_MC_studies_tWb,'r')
+reader = csv.DictReader(csvfile, delimiter=',')#, quotechar='|')
+if not os.path.exists(crab_cfg_path):
+    os.makedirs(crab_cfg_path)
+for row in reader:
+    dataset="'"+row['dataset']+"'"
+    name=row['name']
+    weight=row['weighting on 1fb-1']
+    generator=row['generator']
+    isData=row['isData']
+    isBoostedMiniAOD=row['boosted_dataset']
+    #makeSystematicsTrees=row['makeSystematicsTrees']
+    globalTag=row['globalTag']
+#    if len(dataset)>r3:
+#        requestname="'"+(dataset.split('/')[1])+"'"
+    if dataset!='' and name!='':
+        print 'checking dataset info for',dataset
+        #ckey=das_client.x509()
+        #cert=das_client.x509()
+        #das_client.check_auth(ckey)
+        #das_data=das_client.get_data("https://cmsweb.cern.ch","dataset="+dataset+" instance=phys/prod",0,0,0,300,ckey,cert)
+        das_data=das_client.get_data("file dataset="+dataset+" instance=phys/prod")
+        #print das_data
+        #for d in das_data['data']:
+	    ##print d
+            #for dd in d['dataset']:
+	        ##print dd
+                #print dd['mcm']['nevents']
+
+
+        outfilename=crab_cfg_path+'/crab_'+name+'.py'
+        if os.path.exists(outfilename):
+            for i in range(2,10):
+                outfilename=crab_cfg_path+'/crab_'+name+'_v'+str(i)+'.py'
+                if not os.path.exists(outfilename):
+                    break
+        crabout=open(outfilename,'w')
+        crab_template=open('crab_cfg_BoostedAnalyserWithCrabs_minimal.py','r')
+        for line in crab_template:
+            if 'config.Data.inputDataset' in line:
+                crabout.write('config.Data.inputDataset = '+dataset+'\n')
+            elif 'config.General.requestName' in line:
+                crabout.write('config.General.requestName = "'+name+'"\n')
+            elif 'config.JobType.outputFiles' in line:
+                crabout.write('config.JobType.outputFiles = ["'+name+'_nominal_Tree.root", "'+name+'_nominal_Cutflow.txt"]\n')
+            elif 'config.JobType.pyCfgParams' in line:
+                crabout.write("config.JobType.pyCfgParams = ['outName="+name+"','weight="+weight+"','isData="+isData+"','isBoostedMiniAOD=False','systematicVariations=nominal','LeptonChannel=veto','maxEvents=9999999999','globalTag="+globalTag+"','generatorName="+generator+"']\n")
+            elif 'config.Data.outputDatasetTag' in line:
+                crabout.write("config.Data.outputDatasetTag = 'MC_minimal_"+name+"'\n")
+            else:
+                crabout.write(line)
+        crab_template.close()
+        crabout.close()
